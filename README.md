eeb77271a9b15a5595481521fd8659bb4c435db6
jonghoon <>
2021-05-12 11:17:49 +0900

    [+] readme generator

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # til
 Today I learned
-
-### Go
-* [error 객체 정의](go/error_객체정의.md)
-* [숫자 동등성 체크](go/number_equality.md)
-
-### Mysql
-* [auto increment](mysql/autoincrement.md)
-
-### Swift
-* [swift <-> objective-c 사용하기](swift/swift_objectivec_사용.md)
+### clojure
+### go
+* [error 생성하기](go/error%20생성하기.md)
+* [숫자 동등성 체크](go/숫자%20동등성%20체크.md)
+### mysql
+* [autoincrement](mysql/autoincrement.md)
+### swift
+* [swift 와 objective-c 같이쓰기](swift/swift%20와%20objective-c%20같이쓰기.md)
---
+++ generate.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	deleteReadme()
+
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buffer := bytes.Buffer{}
+	buffer.WriteString("# til\nToday I learned\n")
+
+	for _, f := range files {
+		if f.IsDir() && f.Name()[0] != '.' {
+			generate(f.Name(), &buffer)
+		}
+	}
+
+	err = ioutil.WriteFile("README.md", buffer.Bytes(), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func deleteReadme() {
+	if f, _ := os.Stat("README.md"); f != nil {
+		err := os.Remove("./" + f.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func generate(path string, buffer *bytes.Buffer) {
+	buffer.WriteString("### " + path + "\n")
+
+	fs, err := ioutil.ReadDir("./" + path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range fs {
+		fn := f.Name()
+		n := fn[:len(fn) - 3] // slice '.md' extension
+		buffer.WriteString("* [" + n + "](" + path + "/" + pathString(fn) + ")\n")
+	}
+}
+
+func pathString(str string) string {
+	b := bytes.Buffer{}
+	for _, c := range str {
+		if c == ' ' {
+			b.WriteString("%20")
+		} else {
+			b.WriteRune(c)
+		}
+	}
+	return b.String()
+}
+
+
